bca29246131efcbb6779f56afd8ec5ff8803b731
udayanga91 <u.senanayake91@gmail.com>
2020-06-07 18:04:24 +0900

    Added BaseEntity. Closes #2

---
+++ src/main/java/momo/fashion/api/model/BaseEntity.java
@@ -0,0 +1,26 @@
+package momo.fashion.api.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import java.io.Serializable;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@MappedSuperclass
+public class BaseEntity implements Serializable {
+
+    @Id
+    private Long id;
+
+    public boolean isNew() {
+        return this.id == null;
+    }
+
+}
