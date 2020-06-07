bd222b35021cbd597c4991c2299e80d194843d8e
udayanga91 <u.senanayake91@gmail.com>
2020-06-07 17:27:11 +0900

    Initial commit for web service

---
+++ src/main/java/momo/fashion/api/MomoFashionApiApplication.java
@@ -0,0 +1,13 @@
+package momo.fashion.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MomoFashionApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MomoFashionApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/momo/fashion/api/MomoFashionApiApplicationTests.java
@@ -0,0 +1,13 @@
+package momo.fashion.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MomoFashionApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
