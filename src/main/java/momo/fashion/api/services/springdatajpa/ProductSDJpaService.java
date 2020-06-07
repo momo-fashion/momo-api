325d4e850b186211299a9d8e34e95bedef8914c2
udayanga91 <u.senanayake91@gmail.com>
2020-06-07 19:02:21 +0900

    Added Product Spring Data JPA. Closes #7

---
+++ src/main/java/momo/fashion/api/services/springdatajpa/ProductSDJpaService.java
@@ -0,0 +1,48 @@
+package momo.fashion.api.services.springdatajpa;
+
+import momo.fashion.api.model.Product;
+import momo.fashion.api.repositories.ProductRepository;
+import momo.fashion.api.services.ProductService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class ProductSDJpaService implements ProductService {
+
+    private final ProductRepository productRepository;
+
+    public ProductSDJpaService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    @Override
+    public Set<Product> findAll() {
+        Set<Product> products = new HashSet<>();
+        productRepository.findAll().forEach(products::add);
+        return products;
+    }
+
+    @Override
+    public Product findById(Long aLong) {
+        return productRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Product save(Product object) {
+        return productRepository.save(object);
+    }
+
+    @Override
+    public void delete(Product object) {
+        productRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        productRepository.deleteById(aLong);
+    }
+}
