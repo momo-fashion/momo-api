b645395289c84c676212a2978bbf2d89264c4a6f
udayanga91 <u.senanayake91@gmail.com>
2020-06-07 19:51:25 +0900

    Confugured h2 db. Closes #9 and #10

--- src/main/java/momo/fashion/api/model/Product.java
+++ src/main/java/momo/fashion/api/model/Product.java
@@ -5,16 +5,31 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table(name = "product")
 public class Product extends BaseEntity {
 
+    @Column(name = "name")
     private String name;
+
+    @Column(name = "image_url")
     private String imageUrl;
+
+    @Column(name = "price")
     private Double price;
+
+    @Column(name = "regular_price")
     private Double regularPrice;
+
+    @Column(name = "discount_price")
     private Double discountPrice;
 
 }
